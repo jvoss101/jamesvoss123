ee3dbd80ac10e5df539eaef109d0e730cd6a7a76
jvoss101 <jamesvoss555@hotmail.com>
2022-03-19 07:06:02 -0500

    contacat me pager

---
+++ Contact1/contactform.php
@@ -0,0 +1,14 @@
+if (isset($_POST['submit'])) {
+    $name = $_Post['name'];
+    $subject = $_Post['subject'];
+    $mailFrom = $_Post['mail'];
+    $message = $_Post['message'];
+
+    $mailTo = "James@jamesvoss101.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "you have received an e-mail from ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
